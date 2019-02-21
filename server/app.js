d3769d24b41688b9537f202a168ffd237572c190
hongseonwoo <hongseonwoo@172.30.1.48>
2019-02-22 00:57:13 +0900

    the first javascript example.

---
+++ server/app.js
@@ -0,0 +1,17 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+const http = require('http');
+const os = require('os');
+
+console.log("seonwoo server starting...");
+
+var handler = function(request, response) {
+	console.log("Received request from " + request.connection.remoteAddress);
+	response.writeHead(200);
+	response.end("You've hit "+os.hostname()+"\n");
+};
+
+var www = http.createServer(handler);
+www.listen(8080);
